9aec2b9bf2fe09e0ab02f558c5286a463c17485e
kevin59146 <kevin.onceinfo@gmail.com>
2015-09-22 22:23:07 +0200

    Ajout de la gestion des ajouts et suppressions des listes de course et creation de l activity pour l affiche detaille de la liste

--- app/src/main/java/com/example/once/gestionnairecourses/activity/AccueilActivity.java
+++ app/src/main/java/com/example/once/gestionnairecourses/activity/AccueilActivity.java
@@ -2,13 +2,16 @@ package com.example.once.gestionnairecourses.activity;
 
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.once.gestionnairecourses.R;
 import com.example.once.gestionnairecourses.adapter.ListeCourseAdapter;
@@ -20,13 +23,34 @@ import com.j256.ormlite.dao.RuntimeExceptionDao;
 public class AccueilActivity extends OrmLiteBaseActivity<CourseDbHelper> {
 
     private RuntimeExceptionDao<ListeCourse, Long> daoListeCourse;
-
+    private ListeCourseAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_accueil);
 
-        ListView listeCourses = (ListView) findViewById(R.id.listCourses);
+        final ListView listeCourses = (ListView) findViewById(R.id.listCourses);
+        listeCourses.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ListeCourse listeCourseDelete = (ListeCourse) listeCourses.getItemAtPosition(position);
+                Intent intent = new Intent(AccueilActivity.this,ListeCourseActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        listeCourses.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                ListeCourse listeCourseDelete = (ListeCourse) listeCourses.getItemAtPosition(position);
+                daoListeCourse.delete(listeCourseDelete);
+                adapter.remove(listeCourseDelete);
+                adapter.notifyDataSetChanged();
+
+                return true;
+            }
+        });
+
         daoListeCourse = getHelper().getRuntimeExceptionDao(ListeCourse.class);
         /*ListeCourse listeAuchan = new ListeCourse("Auchan");
         ListeCourse listeLidl = new ListeCourse("Lidl");
@@ -45,7 +69,7 @@ public class AccueilActivity extends OrmLiteBaseActivity<CourseDbHelper> {
         daoArticle.create(saumon);*/
 
 
-        final ListeCourseAdapter adapter = new ListeCourseAdapter(AccueilActivity.this, daoListeCourse.queryForAll());
+        adapter = new ListeCourseAdapter(AccueilActivity.this, daoListeCourse.queryForAll());
 
         listeCourses.setAdapter(adapter);
     }
@@ -82,8 +106,15 @@ public class AccueilActivity extends OrmLiteBaseActivity<CourseDbHelper> {
         builder.setPositiveButton("Ajouter", new DialogInterface.OnClickListener() {
             public void onClick(DialogInterface dialog, int id) {
                 EditText nomListe = (EditText)dialogVue.findViewById(R.id.nomListeDialog);
-                ListeCourse liste = new ListeCourse(nomListe.getText().toString());
-                daoListeCourse.create(liste);
+                String nom = nomListe.getText().toString();
+                if(nom.trim().length() > 0 && nom.trim().length() < 25) {
+                    ListeCourse liste = new ListeCourse(nom);
+                    daoListeCourse.create(liste);
+                    adapter.add(liste);
+                    adapter.notifyDataSetChanged();
+                }else{
+                    Toast.makeText(AccueilActivity.this,"Nom incorrect, la taille doit être comprise entre 0 et 25 caractères !",Toast.LENGTH_LONG).show();
+                }
             }
         });
         builder.setNegativeButton("Annuler", new DialogInterface.OnClickListener() {
